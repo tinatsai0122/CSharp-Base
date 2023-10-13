7121f8128b76c0d1b04e41a2e9604a5c931e0289
Tina Tsai <tinatsai0122@hotmail.com>
2023-10-13 11:53:15 +0200

    adding demo ternary and increment decrement

---
+++ Demo-Operator-IncrementDecrement/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo_Operator_IncrementDecrement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+
+            //int j = i++;
+            int j = i;
+            i++;
+
+            Console.WriteLine($"i={i};j={j}");
+            Console.WriteLine($" i+j => {i+j}");
+
+            i += j;
+            Console.WriteLine($" i :{i}+j:{j} => {i + j}");
+        }
+    }
+}
---
+++ Demo-Operator-Ternary/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_Operator_Ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillex indiquer combien d'annee vous reste-t-il de formation?");
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                //if(year > 1)
+                //{
+                //    Console.WriteLine($"Waouw! Il vous rest {year} annees!");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"Waouw! Il vous rest {year} annee!");
+                //}
+                string s = (year > 1) ? "s" : "";
+                Console.WriteLine($"Waouw! Il vous reste {year } annee{s}");
+            }
+        }
+    }
+}

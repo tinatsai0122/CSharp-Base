58a42fa10d543f4649d52b530f4816e5ca700a00
Tina Tsai <tinatsai0122@hotmail.com>
2023-11-06 14:17:42 +0100

    cross?

---
+++ Exo-struc-02/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exo_struc_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int size = 5;
+            Cross?[,] table = new Cross?[size, size];
+
+            Cross p;
+
+            for (int i = 0; i < size; i++)
+            {
+                p.x = i + 1;
+                p.y = i + 1;
+
+                table[i, i] = p;
+            }
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (table[row, col] is null) Console.Write("\t");
+                    else
+                    {
+                        Console.Write($"X: {table[row, col]?.x} - Y: {table[row, col]?.y}");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Exo-struc-02/Structs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_struc_02
+{
+    public struct Cross
+    {
+        public int x; 
+        public int y;
+    }
+
+
+}

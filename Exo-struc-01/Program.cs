29c97b32de41fd290973b8f8d645ab4cf00db01b
Tina Tsai <tinatsai0122@hotmail.com>
2023-11-06 11:12:26 +0100

    structure (album contain list of tracks including artist, title, length)

---
+++ Exo-struc-01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace Exo_struc_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PisteAudio track1;
+            track1.artist = "Dianne Reeves";
+            track1.title = "Feels So Good";
+            track1.category = "Jazz";
+            track1.lengthSong = 310;
+
+            PisteAudio track2;
+            track2.artist = "Beyonce";
+            track2.title = "Break My Soul";
+            track2.category = "Pop";
+            track2.lengthSong = 280;
+
+            PisteAudio track3;
+            track3.artist = "Ariana Grande";
+            track3.title = "Side to Side";
+            track3.category = "Pop";
+            track3.lengthSong = 350;
+
+            PisteAudio track4 = track3;
+            track4.title = "Stuck with U";
+            track4.lengthSong = 184;
+
+
+            Album album1;
+            album1.albumTitle = "Monday Jazz";
+            album1.releaseDate = new DateOnly(2022,1,1);
+            album1.tracks = new List<PisteAudio>();
+            album1.tracks.Add(track1);
+            album1.tracks.Add(track2);
+            album1.tracks.Add(track3);
+            album1.tracks.Add(track4);
+
+            Console.WriteLine($"Album 1: {album1.albumTitle} released on {album1.releaseDate}: ");
+            Console.WriteLine($"{album1.tracks.Count} tracks");
+            foreach (PisteAudio track in album1.tracks )
+                Console.WriteLine($"\t\"{track.title}\" - sang by {track.artist} ({track.lengthSong}sec.)");
+
+            
+
+
+
+
+            
+
+
+
+
+
+
+
+            Console.WriteLine($"");
+
+
+        }
+    }
+}
---
+++ Exo-struc-01/Structs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_struc_01
+{
+    public struct PisteAudio
+    {
+        public string artist, title, category;
+        public int lengthSong;
+    }
+
+    public struct Album
+    {
+        public string albumTitle;
+        public List<PisteAudio> tracks;
+        public DateOnly releaseDate;
+    }
+}
